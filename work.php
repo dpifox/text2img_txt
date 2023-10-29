529663bdec96fc7fe6fe6f6cf905d4130e542097
冭 <57759622+dpifox@users.noreply.github.com>
2023-10-30 02:41:03 +0800

    Update work.php

--- work.php
+++ work.php
@@ -1,11 +1,27 @@
 <?php
-// 获取查询参数
+// 检查查询参数是否已设置和是否符合要求
+if (!isset($_GET['color']) || !isset($_GET['txt'])) {
+    echo '参数未设置。';
+    exit;
+}
+
 $color = $_GET['color'];
 $txt = $_GET['txt'];
 
+if (!is_numeric($color) && !ctype_alpha($color)) {
+    echo '参数 color 必须为数字或文本。';
+    exit;
+}
+
+if (!is_numeric($txt) && !ctype_alpha($txt)) {
+    echo '参数 txt 必须为数字或文本。';
+    exit;
+}
+
 // 构建Python脚本的命令行参数
 $pythonScript = 'text2img_txt.py';
-$command = "python3 $pythonScript $color $txt";
+// 使用 escapeshellarg 函数确保参数的安全使用
+$command = "python3 $pythonScript " . escapeshellarg($color) . " " . escapeshellarg($txt);
 
 // 执行Python脚本
 $output = shell_exec($command);
